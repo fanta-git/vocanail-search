06e21ae64b57c0e96b6796ee8330c53f381f2c9e
fanta-git <fantaji4957t@gmail.com>
2023-09-08 02:49:48 +0900

    add: 歌詞のソースページを表示

--- src/components/resultItem/LyricTabs.tsx
+++ src/components/resultItem/LyricTabs.tsx
@@ -1,8 +1,11 @@
-import { Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+import { Box, Container, Link, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 
 type Props = {
   video: VideoData;
 };
+
+const langs = ['ja', 'en'] as const;
 
 export default function LyricTabs(props: Props) {
   const { video } = props;
@@ -15,20 +18,23 @@ export default function LyricTabs(props: Props) {
       </TabList>
 
       <TabPanels>
-        <TabPanel>
-          <Container>
-            {video.lyrics.ja.value.split("\n").flatMap((v, i) => (
-              i ? [<br key={i} />, <>{v}</>] : [<>{v}</>]
-            ))}
-          </Container>
-        </TabPanel>
-        <TabPanel>
-          <Container>
-            {video.lyrics.en.value.split("\n").flatMap((v, i) => (
-              i ? [<br key={i} />, <>{v}</>] : [<>{v}</>]
-            ))}
-          </Container>
-        </TabPanel>
+        {
+          langs.map(lang => (
+            <TabPanel key={lang}>
+              <Container>
+                {video.lyrics[lang].value.split("\n").flatMap((v, i) => (
+                  i ? [<br key={i} />, <>{v}</>] : [<>{v}</>]
+                ))}
+                {video.lyrics[lang].source && <Box pt={2}>
+                  <Link href={video.lyrics[lang].url} color={"teal.500"} isExternal>
+                    {video.lyrics[lang].source}
+                    <ExternalLinkIcon mx='2px' />
+                  </Link>
+                </Box>}
+              </Container>
+            </TabPanel>
+          ))
+        }
       </TabPanels>
     </Tabs>
   );
