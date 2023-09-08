531a04dfb3b1a646aca902d854786f2cd75f6056
fanta-git <fantaji4957t@gmail.com>
2023-09-08 20:57:37 +0900

    fix: 警告が出ていたのを消した

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang="ja" title={SITE_NAME}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
